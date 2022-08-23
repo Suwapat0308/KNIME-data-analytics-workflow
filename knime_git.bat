f9090bdbb7d49c99c0fd61296e55da984ba09c46
Suwapat Neangnigon <111272988+Suwapat0308@users.noreply.github.com>
2022-08-23 22:03:25 +0700

    Add files via upload

---
+++ knime_git.bat
@@ -0,0 +1,3 @@
+echo off
+"C:\KNIME\knime_4.5.0\knime_4.5.0\knime.exe" -consoleLog -nosplash -reset -application org.knime.product.KNIME_BATCH_APPLICATION -preferences="C:\KNIME_SERVER\KNIME_AUTO.epf" -workflowDir="C:\Users\KNIME_Auto_report_daily"
+exit /b
